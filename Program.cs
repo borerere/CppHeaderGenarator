c2d58eda010614b663d6c60c85008237180dd7e4
borerere <borerere@gmail.com>
2017-04-21 15:31:04 +0900

    firstcommit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,70 @@ namespace CppHeaderGenarator
     {
         static void Main(string[] args)
         {
+            // 引数で指定されたクラスのヘッダーファイルを作成します。
+            var hfg = new HeaderFileGenarator(args);
+            hfg.Generate();
+        }
+    }
+
+    class HeaderFileGenarator{
+
+
+        public HeaderFileGenarator(string classname)
+        {
+            HeaderFileList.Add(new HeaderFile(classname));
+        }
+        public HeaderFileGenarator(string[] classnamelist)
+        {
+            foreach(string classname in classnamelist)
+            HeaderFileList.Add(new HeaderFile(classname));
+        }
+
+        public void Generate()
+        {
+            foreach(HeaderFile headerfile in HeaderFileList)
+            {
+                string filename = headerfile.Filename;
+                string contents = headerfile.Contents;
+
+                File.WriteAllText(filename, contents);
+            }
+        }
+
+        private List<HeaderFile> HeaderFileList = new List<HeaderFile>();
+
+    }
+    class HeaderFile
+    {
+        public HeaderFile(string filename)
+        {
+            Filename = filename + ".h";
+            Contents = MakeIncludeGard(filename);
+        }
+        public string Filename { get; }
+        public string Contents { get; }
+
+        private string MakeIncludeGard(string filename)
+        {
+            // 拡張子を外す
+            filename.Replace(".h","");
+
+            var IncludeGard = new StringBuilder();
+            IncludeGard.AppendLine("#ifdef  " + filename.ToUpper() + "_HEADER");
+            IncludeGard.AppendLine("#define " + filename.ToUpper() + "_HEADER");
+            IncludeGard.AppendLine();
+            IncludeGard.AppendLine("class " + filename);
+            IncludeGard.AppendLine("{");
+            IncludeGard.AppendLine("public:");
+            IncludeGard.AppendLine("// コンストラクタ&デストラクタ");
+            IncludeGard.AppendLine(filename + "();");
+            IncludeGard.AppendLine("~" + filename + "();");
+            IncludeGard.AppendLine();
+            IncludeGard.AppendLine("};");
+            IncludeGard.AppendLine();
+            IncludeGard.AppendLine("#endif  " + filename.ToUpper() + "_HEADER");
+
+            return IncludeGard.ToString();
         }
     }
 }
